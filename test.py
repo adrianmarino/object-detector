51206836ad17379d7b1648ddf632ddb50ee5093d
Adrian Marino <adrian.marino@restorando.com>
2018-11-04 17:58:36 -0300

    extract frame extraction and output writing

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from lib.video import VideWriterFactory
 from lib.fps_calculator import FpsCalculator
 import numpy as np
 import cv2
-from timeit import default_timer as timer
+import sys
 
 image_height = image_width = 416
 preview_width = 800
@@ -37,19 +37,25 @@ def show_frame(frame, fps_calculator, preview_width):
     cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('frame', resize(frame, width=preview_width))
 
-
-while cv2.waitKey(1) < 0:
-    hasFrame, frame = video_capture.read()
-    if not hasFrame or 0xFF == ord('q'):
-        print("Done processing !!!")
-        print("Output file is stored as ", params['output'])
-        cv2.destroyAllWindows()
-        cv2.waitKey(3000)
-        break
-
-    show_frame(frame, fps_calculator, preview_width)
-    
+def write_output(frame, params):
     if 'input_image' in params:
         cv2.imwrite(params['output'], frame.astype(np.uint8))
     else:
         video_writer.write(frame.astype(np.uint8))
+
+def stop_processing(params):
+    print("Done processing !!!")
+    print("Output file is stored as ", params['output'])
+    cv2.destroyAllWindows()
+    cv2.waitKey(3000)
+    sys.exit(1)
+
+def next_frame(video_capture, params):
+    hasFrame, frame = video_capture.read() 
+    if not hasFrame: stop_processing(params)
+    return hasFrame, frame 
+
+while cv2.waitKey(1) < 0:
+    hasFrame, frame = next_frame(video_capture, params)
+    show_frame(frame, fps_calculator, preview_width)
+    write_output(frame, params)
