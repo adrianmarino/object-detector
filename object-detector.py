06b636f0a95449a5eca1479c299a7bbd2f01d8c7
Adrian Marino <adrian.marino@gmail.com>
2018-11-04 21:44:34 -0300

    ignore files and format code

--- object-detector.py
+++ object-detector.py
@@ -9,17 +9,18 @@ import sys
 
 image_height = image_width = 416
 preview_width = 800
+params = InputParamsResolver().resolve()
+video_capture = VideCaptureFactory().create(params)
+video_writer = VideWriterFactory().create(video_capture, params)
+fps_calculator = FpsCalculator()
 
-params          = InputParamsResolver().resolve()
-video_capture   = VideCaptureFactory().create(params)
-video_writer    = VideWriterFactory().create(video_capture, params)
-fps_calculator  = FpsCalculator()
 
 def write_output(frame, params):
     if 'input_image' in params:
         cv2.imwrite(params['output'], frame.astype(np.uint8))
     else:
         video_writer.write(frame.astype(np.uint8))
+
 
 def stop_processing(params):
     print("Done processing !!!")
@@ -28,10 +29,13 @@ def stop_processing(params):
     cv2.waitKey(3000)
     sys.exit(1)
 
+
 def next_frame(video_capture, params):
     hasFrame, frame = video_capture.read() 
-    if not hasFrame: stop_processing(params)
+    if not hasFrame:
+        stop_processing(params)
     return hasFrame, frame 
+
 
 while cv2.waitKey(1) < 0:
     hasFrame, frame = next_frame(video_capture, params)
